c0042c0dd1a65b9d0dc2a7f8c51c985f5c0da20b
GustavoGomesDias <treeelevengg@gmail.com>
2021-08-05 19:58:34 -0300

    test: add beforeAll and afterAll function

--- src/tests/contact.test.js
+++ src/tests/contact.test.js
@@ -1,6 +1,5 @@
 jest.mock('http');
 const http = require('http');
-const { promisify } = require('util');
 const { request } = jest.requireActual('http');
 
 const contact = {
@@ -10,6 +9,88 @@ const contact = {
   email: 'email@emaal.com',
   github: 'GustavoGomesDias'
 }
+
+beforeAll(async () => {
+  const agent = http.Agent({
+    keepAlive: true,
+    maxSockets: Infinity
+  });
+
+  const options = {
+    agent: agent,
+    hostname: 'localhost',
+    port: 3000,
+    method: 'POST',
+    path: '/',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(JSON.stringify(contact))
+    }
+  }
+
+  const response = new Promise((resolve, reject) => {
+    const req = request(options, (res) => {
+      res.setEncoding('utf8');
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        const { message } = JSON.parse(data);
+        resolve(message);
+      });
+    }).on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+      reject(e);
+    });
+
+    req.write(JSON.stringify(contact));
+    req.end()
+  });
+  await response.then((msg) => msg).catch((err) => err);
+});
+
+afterAll(async () => {
+  const agent = http.Agent({
+    keepAlive: true,
+    maxSockets: Infinity
+  });
+
+  const options = {
+    agent: agent,
+    hostname: 'localhost',
+    port: 3000,
+    method: 'DELETE',
+    path: '/',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(JSON.stringify({ id: 1 }))
+    }
+  }
+
+  const response = new Promise((resolve, reject) => {
+    const req = request(options, (res) => {
+      res.setEncoding('utf8');
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        const { message } = JSON.parse(data);
+        resolve(message);
+      });
+    }).on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+      reject(e);
+    });
+
+    req.write(JSON.stringify({ id: 1 }));
+    req.end()
+  });
+
+  await response.then((msg) => msg).catch((err) => err);
+  
+})
 
 describe("CRUD Contacts", (done) => {
   test("Should get all contacts", async () => {
@@ -37,7 +118,7 @@ describe("CRUD Contacts", (done) => {
           data += chunk;
         });
         res.on('end', () => {
-          resolve({ statusCode: res.statusCode, data: data});
+          resolve({ statusCode: res.statusCode, data: data });
         });
       }).on('error', (e) => {
         console.error(`problem with request: ${e.message}`);
@@ -52,89 +133,89 @@ describe("CRUD Contacts", (done) => {
   });
 
 
-  test("Should create a new contact", async () => {
-    const agent = http.Agent({
-      keepAlive: true,
-      maxSockets: Infinity
-    });
+  // test("Should create a new contact", async () => {
+  //   const agent = http.Agent({
+  //     keepAlive: true,
+  //     maxSockets: Infinity
+  //   });
 
-    const options = {
-      agent: agent,
-      hostname: 'localhost',
-      port: 3000,
-      method: 'POST',
-      path: '/',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(JSON.stringify(contact))
-      }
-    }
+  //   const options = {
+  //     agent: agent,
+  //     hostname: 'localhost',
+  //     port: 3000,
+  //     method: 'POST',
+  //     path: '/',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Content-Length': Buffer.byteLength(JSON.stringify(contact))
+  //     }
+  //   }
 
-    const response = new Promise((resolve, reject) => {
-      const req = request(options, (res) => {
-        res.setEncoding('utf8');
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          const { message } = JSON.parse(data);
-          resolve(message);
-        });
-      }).on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-        reject(e);
-      });
-  
-      req.write(JSON.stringify(contact));
-      req.end()
-    });
+  //   const response = new Promise((resolve, reject) => {
+  //     const req = request(options, (res) => {
+  //       res.setEncoding('utf8');
+  //       let data = '';
+  //       res.on('data', (chunk) => {
+  //         data += chunk;
+  //       });
+  //       res.on('end', () => {
+  //         const { message } = JSON.parse(data);
+  //         resolve(message);
+  //       });
+  //     }).on('error', (e) => {
+  //       console.error(`problem with request: ${e.message}`);
+  //       reject(e);
+  //     });
 
-    const msg = await response.then((msg) => msg).catch((err) => done(err));
+  //     req.write(JSON.stringify(contact));
+  //     req.end()
+  //   });
 
-    expect(msg).toEqual('Usuário cadastrado.');
-  });
+  //   const msg = await response.then((msg) => msg).catch((err) => done(err));
 
-  test("Should delete a contac by id", async () => {
-    const agent = http.Agent({
-      keepAlive: true,
-      maxSockets: Infinity
-    });
+  //   expect(msg).toEqual('Contato cadastrado com sucesso.');
+  // });
 
-    const options = {
-      agent: agent,
-      hostname: 'localhost',
-      port: 3000,
-      method: 'POST',
-      path: '/',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(JSON.stringify({ id: 1 }))
-      }
-    }
+  // test("Should delete a contac by id", async (done) => {
+  //   const agent = http.Agent({
+  //     keepAlive: true,
+  //     maxSockets: Infinity
+  //   });
 
-    const response = new Promise((resolve, reject) => {
-      const req = request(options, (res) => {
-        res.setEncoding('utf8');
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          const { message } = JSON.parse(data);
-          resolve(message);
-        });
-      }).on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-        reject(e);
-      });
-  
-      req.write(JSON.stringify({ id: 1 }));
-      req.end()
-    });
+  //   const options = {
+  //     agent: agent,
+  //     hostname: 'localhost',
+  //     port: 3000,
+  //     method: 'DELETE',
+  //     path: '/',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Content-Length': Buffer.byteLength(JSON.stringify({ id: 1 }))
+  //     }
+  //   }
 
-    const msg = await response.then((msg) => msg).catch((err) => done(err));
+  //   const response = new Promise((resolve, reject) => {
+  //     const req = request(options, (res) => {
+  //       res.setEncoding('utf8');
+  //       let data = '';
+  //       res.on('data', (chunk) => {
+  //         data += chunk;
+  //       });
+  //       res.on('end', () => {
+  //         const { message } = JSON.parse(data);
+  //         resolve(message);
+  //       });
+  //     }).on('error', (e) => {
+  //       console.error(`problem with request: ${e.message}`);
+  //       reject(e);
+  //     });
 
-    expect(msg).toEqual('Usuário deletado.');
-  });
+  //     req.write(JSON.stringify({ id: 1 }));
+  //     req.end()
+  //   });
+
+  //   const msg = await response.then((msg) => msg).catch((err) => done(err));
+
+  //   expect(msg).toEqual('Contato deletado com sucesso.');
+  // });
 });
